f0874f64bf34999023a6bed7003e04920fe7e13c
1355165054@qq.com <1355165054@gmail.com>
2024-10-28 10:30:51 +0800

    2

---
+++ src/app/s57parser/main.cpp
@@ -0,0 +1,14 @@
+#include <QApplication>
+#include <QMenuBar>
+
+
+
+int main(int argc, char * argv[])
+{
+    QApplication a(argc, argv);
+
+
+
+    int ret = a.exec();
+    return ret;
+}
---
+++ src/app/s57parser/parser.h
@@ -0,0 +1,13 @@
+#ifndef parser_h__20241028
+#define parser_h__20241028
+
+#include <QString>
+
+class Parser
+{
+
+
+};
+
+
+#endif
